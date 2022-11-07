99de313dcce5ccde1c98fc43f4b745cd428c625f
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-07 19:14:20 +0700

    Making Controller

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("Express + TypeScript Server");
--- controller/pesan.js
+++ controller/pesan.js
@@ -10,16 +10,90 @@ const PostPesan = async (req, res) => {
 			isi_pesan: pesan,
 		});
 
-		res.send({
+		res.json({
 			message: "Pesan berhasil dikirim",
 			data: pesanBaru,
 		});
 	} catch (error) {
-		res.send({
+		res.json({
 			message: "Pesan gagal dikirim",
 			data: error,
 		});
 	}
 };
 
-module.exports = {};
+const DeletePesan = async (req, res) => {
+	const { id } = req.params;
+
+	try {
+		const pesanDihapus = await db.Pesan.destroy({
+			where: {
+				id_pesan: id,
+			},
+		});
+
+		res.json({
+			message: "Pesan berhasil dihapus",
+			data: pesanDihapus,
+		});
+	} catch (error) {
+		res.json({
+			message: "Pesan gagal dihapus",
+			data: error,
+		});
+	}
+};
+
+const UpdatePesan = async (req, res) => {
+	const { id } = req.params;
+	const { nama, email, pesan } = req.body;
+
+	try {
+		const pesanDiupdate = await db.Pesan.update(
+			{
+				nama_pengirim: nama,
+				email_pengirim: email,
+				isi_pesan: pesan,
+			},
+			{
+				where: {
+					id_pesan: id,
+				},
+			}
+		);
+
+		res.json({
+			message: "Pesan berhasil diupdate",
+			data: pesanDiupdate,
+		});
+	} catch (error) {
+		res.json({
+			message: "Pesan gagal diupdate",
+			data: error,
+		});
+	}
+};
+
+const GetPesan = async (req, res) => {
+	let allPesan;
+	try {
+		allPesan = await db.Pesan.findAll();
+	} catch (error) {
+		res.json({
+			message: "Pesan gagal didapatkan",
+			data: error,
+		});
+	}
+
+	res.render("pesan", {
+		title: "Pesan",
+		pesan: allPesan,
+	});
+};
+
+module.exports = {
+	PostPesan,
+	DeletePesan,
+	UpdatePesan,
+	GetPesan,
+};
