c4206b35d227de33623d87ebd9303ab2f3a475ce
Azie Melza Pratama <aziemp66@gmail.com>
2022-11-08 08:45:55 +0700

    Serving Static Files

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "view"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", pesanRouter);
+app.use("/pesan", pesanRouter);
 
 db.sequelize.sync().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+	app.listen(port, () => {
+		console.log(`Server is running on port ${port}`);
+	});
 });
